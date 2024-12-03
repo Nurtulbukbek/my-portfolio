5e314580712c66dece9f3441d417cf20c558f545
Resul <owezresul0@gmail.com>
2024-12-03 16:31:54 +0500

    change tg

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -19,7 +19,7 @@ function Contacts() {
                             <span className="yellowText">or</span>
                         </h1>
                         <div className="socialMedia">
-                            <img src="../telegram.svg" alt="" className="socialItem" />
+                            <a href="https://t.me/nurtulbek"><img src="../telegram.svg" alt="" className="socialItem" /></a>
                             <a href="https://www.instagram.com/nurtulbek/profilecard/?igsh=MXU5ZW5weDhqNmpzZw=="><img src="../instagram.svg" alt="" className="socialItem" /></a>
                             <a href="https://github.com/Nurtulbukbek"><img src="../github.svg" alt="" className="socialItem" /></a>
                         </div>
